33f0888835b418421da9f89a84dd3e49e2186931
guo-yong-zhi <guo-yong-zhi@outlook.com>
2021-09-01 15:36:02 +0800

    Delete stop.sh

--- stop.sh
+++
@@ -1,5 +0,0 @@
-PORT=2222
-cat KOSSH.pid | xargs kill
-iptables -D INPUT -p tcp --dport "$PORT" -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -D OUTPUT -p tcp --sport "$PORT" -m conntrack --ctstate ESTABLISHED -j ACCEPT
-killall dropbear
