2da59940fda844ebf9398220badb047844580632
guo-yong-zhi <55872791+guo-yong-zhi@users.noreply.github.com>
2022-05-13 13:07:10 +0800

    align with filebrowser version

--- KOSSH.sh
+++ KOSSH.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+col=30
 PORT=`cat PORT`
 [ "$PORT" == "" ] && PORT=2222
 
@@ -5,17 +7,21 @@ iptables -A INPUT -p tcp --dport "$PORT" -m conntrack --ctstate NEW,ESTABLISHED 
 iptables -A OUTPUT -p tcp --sport "$PORT" -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
 # enable wireless if it is currently off
-WIFI_IS_OFF=0
+WIFI_WAS_OFF=0
 if [ 0 -eq `lipc-get-prop com.lab126.cmd wirelessEnable` ]; then
-	eips 30 3 "WiFi is off, turning it on now"
+	eips $col 3 "WiFi is off, turning it on now"
 	lipc-set-prop com.lab126.cmd wirelessEnable 1
-	WIFI_IS_OFF=1
+	WIFI_WAS_OFF=1
 fi
 
 # refresh IP display in the background
 while :; do
-    eips 25 0 "ssh root@`ifconfig wlan0 | grep 'inet addr' | awk -F '[ :]' '{print $13}'` -p$PORT"
+    eips $((col-5)) 0 "ssh root@`ifconfig wlan0 | grep 'inet addr' | awk -F '[ :]' '{print $13}'` -p$PORT"
     sleep 3
+    if [ 0 -eq `lipc-get-prop com.lab126.cmd wirelessEnable` ]; then
+    	lipc-send-event com.lab126.hal powerButtonPressed
+    	break
+    fi
 done > /dev/null &
 
 
@@ -23,33 +29,33 @@ done > /dev/null &
 #ref: koreader/plugins/SSH.koplugin/main.lua
 
 # waiting for powerButtonPressed
-PSS_IS_OFF=0
+PSS_WAS_OFF=0
 if [ 0 -eq `lipc-get-prop com.lab126.powerd preventScreenSaver` ]; then
 	lipc-set-prop -i com.lab126.powerd preventScreenSaver 1
-	PSS_IS_OFF=1
+	PSS_WAS_OFF=1
 fi
 lipc-wait-event  com.lab126.hal powerButtonPressed | read event #it's blocking
 # Restore PSS status
-if [ 1 -eq $PSS_IS_OFF ]; then
+if [ 1 -eq $PSS_WAS_OFF ]; then
 	lipc-set-prop -i com.lab126.powerd preventScreenSaver 0
 fi
 
 kill $(jobs -p)
-eips 30 3 "ssh server is turned off      "
+eips $col 3 "ssh server is turned off      "
 
 # Restore WiFi status
-if [ 1 -eq $WIFI_IS_OFF ]; then
+if [ 1 -eq $WIFI_WAS_OFF ] && [ 1 -eq `lipc-get-prop com.lab126.cmd wirelessEnable` ]; then
 	lipc-set-prop com.lab126.cmd wirelessEnable 0
 	sleep 0.5
-	eips 30 3 "Turning off WiFi              "
+	eips $col 3 "Turning off WiFi              "
 fi
 
 # Clear screen
 sleep 1
-eips 25 0 "                               "
-eips 30 3 "                              "
+eips $((col-5)) 0 "                               "
+eips $col 3 "                              "
 
-# stop ssh
+# stop tasks
 cat KOSSH.pid | xargs kill
 iptables -D INPUT -p tcp --dport "$PORT" -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -D OUTPUT -p tcp --sport "$PORT" -m conntrack --ctstate ESTABLISHED -j ACCEPT
